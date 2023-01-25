b0c6a094c1b63f0aa78bae84f2fef3a439d5efb9
Gersaurio <vgerson2295@gmail.com>
2023-01-24 23:56:15 -0500

    Ruta Users and Songs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+// Importing Prisma Client
+import {PrismaClient} from '@prisma/client';
 
 
 dotenv.config();
 
+const prisma = new PrismaClient();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
+
+
+
+
 app.get("/", (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server + Testing Prisma');
+})
+
+app.get("/api/v1/users", (req: Request,res: Response) => {
+    res.send('Creación con password hasheado-POST');
+})
+
+app.get("/api/v1/songs", (req: Request,res: Response) => {
+    res.send('Lee todas las canciones');
+})
+
+app.get("/api/v1/songs", (req: Request,res: Response) => {
+    res.send('Login de usuario');
 })
 
 app.listen(port, () => {
